ec551426655a0f81b91ca272d10c7bc8d645e555
Kiyana Salkeld <kiyana.salkeld@berkeley.edu>
2014-03-08 13:03:40 -0800

    second commit

---
+++ gen/edu/berkeley/Sync_Up_Itetation_1/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package edu.berkeley.Sync_Up_Itetation_1;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
